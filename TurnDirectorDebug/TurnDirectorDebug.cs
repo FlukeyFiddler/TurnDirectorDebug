cc59f9511d2c97a91150b0ae736259f2e3ed62a2
FlukeyFiddler <robertovdmeer@gmail.com>
2018-06-15 12:03:17 +0200

    Initial commit

---
+++ TurnDirectorDebug/TurnDirectorDebug.cs
@@ -0,0 +1,21 @@
+﻿using Harmony;
+using nl.flukeyfiddler.bt.TurnDirectorDebug.Utils;
+using nl.flukeyfiddler.bt.Utils.Logger;
+using System.IO;
+using System.Reflection;
+
+namespace nl.flukeyfiddler.bt.TurnDirectorDebug
+{
+    public class TurnDirectorDebug
+    {
+        public static void Init(string modDirectory, string settingsJSON)
+        {
+            var harmony = HarmonyInstance.Create("nl.flukeyfiddler.bt.MyMod");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+
+
+            Logger.SetLogFilePath(new LogFilePath(Path.Combine(modDirectory, "Log.txt")));
+            Logger.GameStarted();
+        }
+    }
+}
---
+++ TurnDirectorDebug/Utils/Logger.cs
@@ -0,0 +1,51 @@
+﻿using nl.flukeyfiddler.bt.Utils.Logger;
+using System;
+using System.Reflection;
+
+namespace nl.flukeyfiddler.bt.TurnDirectorDebug.Utils
+{
+    public static class Logger
+    {
+        private static LogFilePath logFilePath;
+        
+        public static void SetLogFilePath(LogFilePath logFilePath)
+        {
+            Logger.logFilePath = logFilePath;
+        }
+
+        public static void Error(Exception ex, MethodBase caller = null)
+        {
+            LoggerUtil.Error(logFilePath, ex, caller);
+        }
+
+        public static void Line(string line, MethodBase caller = null)
+        {
+            LoggerUtil.Line(logFilePath, line, caller);
+        }
+
+        public static void InfoLine(MethodBase caller = null)
+        {
+            LoggerUtil.InfoLine(logFilePath, caller);
+        }
+
+        public static void EndLine()
+        {
+            LoggerUtil.EndLine(logFilePath);
+        }
+
+        public static void Minimal(string line)
+        {
+            LoggerUtil.Minimal(logFilePath, line);
+        }
+
+        public static void Block(string[] lines, MethodBase caller = null)
+        {
+            LoggerUtil.Block(logFilePath, lines, caller);
+        }
+
+        public static void GameStarted()
+        {
+            LoggerUtil.GameStarted(logFilePath);
+        }
+    }
+}
